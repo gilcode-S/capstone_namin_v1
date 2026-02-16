c7fc86c90a6bc511ab79298772eebbd65df565a2
Gilbert Engalan <gilbertpude2003@gmail.com>
2026-02-17 00:26:11 +0800

    add role in users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['hr', 'registrar', 'staff', 'super admin'])->default('hr');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
